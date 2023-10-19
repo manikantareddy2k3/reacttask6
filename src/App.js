6f7bad24ce3547a4f1dbc00a642912d4d41044e0
Manikanta Lacha Reddy <manikantareddy2k3@gmail.com>
2023-10-19 15:00:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// app.js
+
+// Add an event listener to the "Download CV" button
+const cvButton = document.querySelector('.cv-button');
+
+cvButton.addEventListener('click', () => {
+    // Replace this alert with any desired action
+    alert('CV Download functionality will be implemented here.');
+});
